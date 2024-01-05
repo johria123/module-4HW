52198aac8c7e712a4049ed06acf9dae4078a880b
johria <johria.mehboob@gmail.com>
2024-01-04 21:43:47 -0600

    done

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 # My-Portfolio
 
-This project was a way of giving a brief introduction about myself. This project went over some interesting infomation about me, my work history, and my contact infomation. 
+This challenge was a good way of getting comfortable with many factors that we have learned in class. Getting comfortable wiht JavaScript and css and index.HTML. 
 
 ## Features
-    About me
-    Work history
-    Contact information
+    The quiz contains questions, answers, and a timer to keep track of how long it takes the user to finish the quiz. The user will have a maximum time of 75 seconds to finish the quiz that contains 4 questions.
 
 ## usuage
-    Explore the portfolio about me and my prior work experience. Also, a good way of contacting me if any information is needed or any opportunities are good for me.
+    This was a good way of getting practice on how to make a quiz, this was a good way of creating a good dinky app that functions at the best of my knowledge. 
 
 ###
-    Technology that was used for this portfolio was HTML, CSS, reset.css, and GitHub.
+    Technology that was used for this portfolio was HTML, CSS, reset.css, javaScript and GitHub.
 
-##
-    contact infomation is provided for your reference
-        Email: johria.mehboob@gmail.com
-        LinkedIn: www.linkedin.com/in/johria-mehboob-
+![screenshot](./assets/IMG_0676.jpg)
+[Do visit my page](https://github.com/johria123/module-4HW)
---
+++ assets/js/script.js
@@ -0,0 +1,98 @@
+var quizContainer = document.getElementById("quizContainer");
+var questionContainer = document.getElementById("question-container");
+var nextButton = document.getElementById("next-btn");
+var timerDisplay = document.getElementById("timer-display");
+var submitButton = document.getElementById("submit-btn")
+
+var questions = [
+    {
+        question: "Commonly used data types DO Not include?",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correctAnswer: "3. alerts"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["1. javascript", "2. terminal", "3. for loops", "4. console.log"],
+        correctAnswer: "4. console.log"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with _______.",
+        options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        correctAnswer: "2. curly brackets"
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        correctAnswer: "3. quotes"
+    }
+];
+
+var currentQuestionIndex = 0;
+var timerSeconds = 75;
+var timerInterval;
+
+function startQuiz() {
+    showQuestion();
+    startTimer();
+
+}
+
+function startTimer() {
+    timerInterval = setInterval(function () {
+        timerDisplay.textContent = `Time left: ${timerSeconds} seconds`;
+
+        if (timerSeconds <= 0) {
+            clearInterval(timerInterval);
+            quizContainer.innerHTML = '<h1>Time is up! Quiz Completed</h1>';
+        } else {
+            timerSeconds--;
+        }
+    }, 1000);
+}
+
+
+function showQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    questionContainer.innerHTML = `<h2>${currentQuestion.question}</h2>`;
+
+    currentQuestion.options.forEach(option => {
+        questionContainer.innerHTML += `<label><input type="radio" name="answer" value="${option}"> ${option}</label><br>`;
+    });
+}
+
+
+function nextQuestion() {
+   
+    var selectedOption = document.querySelector('input[name="answer"]:checked');
+
+    
+    if (selectedOption) {
+       
+        if (selectedOption.value === questions[currentQuestionIndex].correctAnswer) {
+
+            console.log('right!');
+        } else {
+
+            console.log('wrong!');
+        }
+
+        currentQuestionIndex++;
+
+        if (currentQuestionIndex < questions.length) {
+          
+            showQuestion();
+        } else {
+           
+            clearInterval(timerInterval);
+            quizContainer.innerHTML = '<h1>Quiz Completed!</h1>';
+        }
+    } else {
+       
+        alert('Please select an option!');
+    }
+}
+
+startQuiz();
+
+nextButton.addEventListener('click', nextQuestion);
+submitButton.addEventListener("click", submitQuiz);
